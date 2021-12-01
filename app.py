bc0f3698b1f188b5efb45dbeced18a9625b104d5
ylg619 <laghrari.yassine@gmail.com>
2021-12-01 16:24:18 +0100

    first commit

--- app.py
+++ app.py
@@ -2,19 +2,14 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
-import os
 
-@st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True,suppress_st_warning=True)
 def load_model_cache():
-    PATH = os.path.join(os.path.curdir,'autoencoder.h5')
-    model=None
-    if os.path.exists(PATH):
-        model = load_model(PATH)
+    model = load_model('autoencoder.h5')
     return model
 
 model = load_model_cache()
-if model is None:
-    print("File not Found")
+
 
 uploaded_file = st.file_uploader("Gimme  image", type=["png", "jpg", "jpeg"])
 res = None
@@ -23,14 +18,13 @@ image=None
 if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image)
-    imgArray = np.array(image)
+    rgb_im = image.convert('RGB')
+    imgArray = np.array(rgb_im)
 
 if st.button('Predict'):
     # Send to API, endpoint must accept POST
     if image is not None:
-        np.array(imgArray.reshape((28,28))).shape
-        res = model.predict(np.array([imgArray.reshape((28,28))])/255)
-    if res is not None:
-        
-       #res_arr = Image.fromarray(res[0])
-        st.image(res[0])
+        res = model.predict(np.array(imgArray)/255)
+    if res:
+        res = Image.fromarray(res[0])
+        st.image(res)
